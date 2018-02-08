7994ff20ac127b30a77a4e467ee1fa5cabf97db5
mrda666 <33660417+mrda666@users.noreply.github.com>
2018-02-08 18:30:06 +0100

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,7 @@
+
+define('AUTOSAVE_INTERVAL', 300 ); // default is 60 (seconds)
+define('WP_POST_REVISIONS', false );
+define( 'DISALLOW_FILE_EDIT', true );
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
